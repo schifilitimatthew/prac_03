e945af5b9aa0f72ff91652bdc16629e9f13b905e
schifillitimatthew <schifillitimatthew28@gmail.com>
2020-08-27 20:37:39 +1000

    Update broken score program (part B)

--- broken_score.py
+++ broken_score.py
@@ -3,18 +3,24 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    elif score > 90:
-        print("Excellent")
-    elif score < 50:
-        print("Bad")
+def main():
+    """Get a score and display its status."""
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
     elif score >= 50:
-        print("Passable")
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
 
